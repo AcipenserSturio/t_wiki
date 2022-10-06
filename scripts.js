e3d1bb8bae777e22804b0205e478aa550ec51de1
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2022-10-06 21:47:57 +0300

    Random link

--- scripts.js
+++ scripts.js
@@ -6,6 +6,14 @@ function Get(yourUrl) {
   return Httpreq.responseText;
 }
 
+function get_random_link() {
+  return links[Math.floor(Math.random() * links.length)];
+}
 
-wiki_tag_contents = "./wiki.yna"
-console.log(Get(wiki_tag_contents))
+function go() {
+  window.location = get_random_link();
+}
+
+wiki_tag = "./wiki.yna"
+links = Get(wiki_tag).replace("​{choose:https:;}//en.wikipedia.org/wiki/{choose:", "").replace("{!!extend end pre=";" post="" public=true};}", "").split(";")
+console.log(links)
